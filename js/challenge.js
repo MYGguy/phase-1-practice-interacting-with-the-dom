840979d19d088be6d743e1553f3d4e2506aea8ba
Moshe Goldstein <mygguy613@gmail.com>
2025-01-21 16:37:00 -0500

    add plus minus functionality

--- js/challenge.js
+++ js/challenge.js
@@ -1,3 +1,6 @@
 const counter = document.querySelector('#counter');
 
 setInterval(() => counter.textContent++, 1000);
+
+document.querySelector('#plus').addEventListener('click', () => counter.textContent++)
+document.querySelector('#minus').addEventListener('click', () => counter.textContent--)
