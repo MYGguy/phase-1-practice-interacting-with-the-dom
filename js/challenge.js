2314f4fccfbd9f8fe4c017e2cb0fd74ff6ce782f
Moshe Goldstein <mygguy613@gmail.com>
2025-01-21 17:13:28 -0500

    remove liker and make again

--- js/challenge.js
+++ js/challenge.js
@@ -3,9 +3,5 @@ const counter = document.querySelector('#counter');
 setInterval(() => counter.textContent++, 1000);
 
 document.querySelector('#plus').addEventListener('click', () => counter.textContent++)
-document.querySelector('#minus').addEventListener('click', () => counter.textContent--)
+document.querySelector('#minus').addEventListener('click', () => counter.textContent--);
 
-document.querySelector('#heart').addEventListener('click', () => {
-    document.querySelector('.likes').innerHTML += `<li>${counter.textContent} has been liked ${'##'
-        } times.</li>`
-})
