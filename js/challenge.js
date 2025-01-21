807525c910c0d357e6a61cca5f9c37755eb0ccca
Moshe Goldstein <mygguy613@gmail.com>
2025-01-21 16:43:03 -0500

    add liker part 1

--- js/challenge.js
+++ js/challenge.js
@@ -4,3 +4,8 @@ setInterval(() => counter.textContent++, 1000);
 
 document.querySelector('#plus').addEventListener('click', () => counter.textContent++)
 document.querySelector('#minus').addEventListener('click', () => counter.textContent--)
+
+document.querySelector('#heart').addEventListener('click', () => {
+    document.querySelector('.likes').innerHTML += `<li>${counter.textContent} has been liked ${'##'
+        } times.</li>`
+})
